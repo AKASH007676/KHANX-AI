95f3bfa0f8f90d4e71dadd286de01f3011014144
AKASH007676 <158271327+AKASH007676@users.noreply.github.com>
2024-12-21 23:34:25 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "fioFgC4K#SH2_s0dKZYZSkuCeoxaO8qAh-8fZ4RxJ69WsLzr__nQ",
+SESSION_ID: process.env.SESSION_ID || "a6Zjmb7S#on5wYLyBSo4jQ8PuNmtY326zWsMiNstOoxo5xPS-5HI",
 // add your Session Id 
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot
@@ -13,7 +13,7 @@ BOT_NAME: process.env.BOT_NAME || "A ᴋ ꫝ ꜱ ʜ ツ",
 // add bot namw here for menu
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "917872324809",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "917908178746",
 // add your bot owner number
 OWNER_NAME: process.env.OWNER_NAME || "A ᴋ ꫝ ꜱ ʜ ツ",
 // add bot owner name
@@ -25,13 +25,13 @@ LIVE_MSG: process.env.LIVE_MSG || "> Zinda Hun Yar *A ᴋ ꫝ ꜱ ʜ ツ*⚡",
 // add alive msg here 
 READ_MESSAGE: process.env.READ_MESSAGE || "false",
 // Turn true or false for automatic read msgs
-AUTO_REACT: process.env.AUTO_REACT || "true",
+AUTO_REACT: process.env.AUTO_REACT || "false",
 // make this true or false for auto react on all msgs
 ANTI_BAD: process.env.ANTI_BAD || "false",
 // false or true for anti bad words
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
-AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "true",
+AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
 // make true if you want auto reply on status 
 AUTO_STATUS__MSG: process.env.AUTO_STATUS__MSG || "*SEEN YOUR STATUS BY A ᴋ ꫝ ꜱ ʜ ツ🤍*",
 // set the auto reply massage on status reply    
@@ -43,11 +43,11 @@ AUTO_VOICE: process.env.AUTO_VOICE || "off",
 // make true for send automatic voices
 AUTO_STICKER: process.env.AUTO_STICKER || "false",
 // make true for automatic stickers 
-AUTO_REPLY: process.env.AUTO_REPLY || "true",
+AUTO_REPLY: process.env.AUTO_REPLY || "falsw",
 // make true or false automatic text reply 
 HEART_REACT: process.env.HEART_REACT || "false",
 // make this true or false for heart reactions only 
-OWNER_REACT: process.env.OWNER_REACT || "true",
+OWNER_REACT: process.env.OWNER_REACT || "false",
 // make it true or fasle for only react on owner msg only 
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "false",
 // maks true for always online 
